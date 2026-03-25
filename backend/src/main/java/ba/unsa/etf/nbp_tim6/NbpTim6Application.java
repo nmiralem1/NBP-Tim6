97f8d7a2fa591221deebbb6012a68d23390eeb78
azunic3 <azunic3@etf.unsa.ba>
2026-03-25 14:01:43 +0100

    CORS added

--- backend/src/main/java/ba/unsa/etf/nbp_tim6/NbpTim6Application.java
+++ backend/src/main/java/ba/unsa/etf/nbp_tim6/NbpTim6Application.java
@@ -2,6 +2,9 @@ package ba.unsa.etf.nbp_tim6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NbpTim6Application {
@@ -9,5 +12,18 @@ public class NbpTim6Application {
 	public static void main(String[] args) {
 		SpringApplication.run(NbpTim6Application.class, args);
 	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
 }
